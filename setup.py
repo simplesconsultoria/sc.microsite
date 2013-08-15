308671c311b27bbc79ac2bc324ebb1e39d6d71c0
hvelarde <hector.velarde@gmail.com>
2013-08-15 17:33:49 -0300

    Preparing release 1.0b1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0b1.dev0'
+version = '1.0b1'
 description = "A very basic Dexterity-based container to be used as a microsite.",
 long_description = (
     open("README.rst").read() + "\n" +
