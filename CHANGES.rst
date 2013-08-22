14a9ebabe3db34072bc966bfb9266a5ae39647dd
Juan Pablo Giménez <jpg@rosario.com>
2013-08-22 13:18:48 -0300

    Fixes redmine issue #7604, https://simplesnet.com.br/suporte/issues/7604 Basic ISelectableConstrainTypes support, we need to fully implement it.

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,6 +4,8 @@ There's a frood who really knows where his towel is
 1.0b2 (unreleased)
 ------------------
 
+- Basic ISelectableConstrainTypes support. [jpgimenez]
+
 - Update view methods, sc.microsite content has the same methods than a folder. [jpgimenez]
 
 - Default view changed to folder_listing. [jpgimenez]
--- src/sc/microsite/content.py
+++ src/sc/microsite/content.py
@@ -3,9 +3,29 @@
 from five import grok
 from plone.dexterity.content import Container
 from sc.microsite.interfaces import IMicrosite
+from Products.CMFPlone.interfaces.constrains import ISelectableConstrainTypes
+from Products.CMFCore.utils import getToolByName
 
 
 class Microsite(Container):
     """A microsite.
     """
     grok.implements(IMicrosite)
+
+    def getLocallyAllowedTypes(self):
+        """
+        By now we allow all allowed types without constrain.
+        TODO: fully implement ISelectableConstrainTypes
+        """
+        portal_types = getToolByName(self, 'portal_types')
+        my_type = portal_types.getTypeInfo(self)
+        result = portal_types.listTypeInfo()
+        return [t for t in result if my_type.allowType(t.getId()) and
+                t.isConstructionAllowed(self)]
+
+    def getImmediatelyAddableTypes(self, context=None):
+        """
+        By now we allow all allowed types without constrain.
+        TODO: fully implement ISelectableConstrainTypes
+        """
+        return self.getLocallyAllowedTypes()
