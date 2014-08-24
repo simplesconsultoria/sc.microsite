09444ecbcef7e57c1a41061e0061ede9b02e3d7a
Érico Andrei <erico@simplesconsultoria.com.br>
2014-08-24 15:50:38 -0300

    Refactor hiding profiles and products

--- src/sc/microsite/config.py
+++ src/sc/microsite/config.py
@@ -1,3 +1,32 @@
 # -*- coding: utf-8 -*-
+from Products.CMFQuickInstallerTool import interfaces as qi_interfaces
+from Products.CMFPlone import interfaces as st_interfaces
+from zope.interface import implements
 
 PROJECTNAME = 'sc.microsite'
+
+HIDDEN_PRODUCTS = [
+    'sc.microsite.upgrades.v1000',
+]
+
+HIDDEN_PROFILES = [
+    'sc.microsite:uninstall',
+]
+
+
+class HiddenProducts(object):
+
+    implements(qi_interfaces.INonInstallable)
+
+    def getNonInstallableProducts(self):
+        products = []
+        products = [p for p in HIDDEN_PRODUCTS]
+        return products
+
+
+class HiddenProfiles(object):
+
+    implements(st_interfaces.INonInstallable)
+
+    def getNonInstallableProfiles(self):
+        return HIDDEN_PROFILES
