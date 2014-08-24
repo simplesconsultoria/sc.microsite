35056c83cd02f8223454c3f71712b4687f86ad35
Érico Andrei <erico@simplesconsultoria.com.br>
2014-08-24 17:31:57 -0300

    Move upgrade step code to the right place. Add tests

--- src/sc/microsite/tests/test_setup.py
+++ src/sc/microsite/tests/test_setup.py
@@ -21,6 +21,7 @@ class BaseTestCase(unittest.TestCase):
         self.portal = self.layer['portal']
         self.qi = self.portal['portal_quickinstaller']
         self.st = self.portal['portal_setup']
+        self.tt = self.portal['portal_types']
 
 
 class InstallTestCase(BaseTestCase):
@@ -87,13 +88,31 @@ class TestUpgrade(BaseTestCase):
         steps = self.get_upgrade_steps(source, dest)
         # Execute them
         for step in steps:
+            if isinstance(step, list):
+                step = step[0]
             step['step'].doStep(self.st)
 
     def test_to1000_available(self):
-        source = '1'
-        dest = '1000'
+        source, dest = ('1', '1000')
         steps = self.get_upgrade_steps(source, dest)
         self.assertEqual(len(steps), 1)
+
+    def test_to1000_enable_ipublication(self):
+        source, dest = ('1', '1000')
+        behavior = 'plone.app.dexterity.behaviors.metadata.IPublication'
+        fti = self.tt['sc.microsite']
+        if behavior in fti.behaviors:
+            # Remove behavior if it is already there
+            behaviors = list(fti.behaviors)
+            behaviors.remove(behavior)
+            fti.behaviors = tuple(behaviors)
+        self.run_upgrade(source, dest)
+        # Get the fti again
+        fti = self.tt['sc.microsite']
+        self.assertIn(
+            behavior,
+            fti.behaviors
+        )
 
 
 class UninstallTestCase(BaseTestCase):
--- src/sc/microsite/upgrades/v1000/handler.py
+++ src/sc/microsite/upgrades/v1000/handler.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
-
+from plone import api
 from sc.microsite.config import PROJECTNAME
 
 import logging
 
+logger = logging.getLogger(PROJECTNAME)
 
-def do_upgrade(context):
-    """Upgrade to version 1000
+
+def enable_ipublication(context):
+    """Enable IPublication behavior
     """
-    logger = logging.getLogger(PROJECTNAME)
-    logger.info('Upgraded to version 1000')
+    behavior = 'plone.app.dexterity.behaviors.metadata.IPublication'
+    portal_types = api.portal.get_tool('portal_types')
+    fti = portal_types['sc.microsite']
+    behaviors = fti.behaviors
+    if behavior not in behaviors:
+        behaviors = list(behaviors)
+        behaviors.append(behavior)
+        fti.behaviors = tuple(behaviors)
+    logger.info('IPublication behavior applied to sc.microsite')
