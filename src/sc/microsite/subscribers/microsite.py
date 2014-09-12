bdcccfa7f598766f447850e7471b26fac9dbb154
hvelarde <hector.velarde@gmail.com>
2014-09-12 11:24:11 -0300

    Fix add permission
    - By default, add permission should be granted to Site Administrator and Contributor roles, and not to Editor rol
    - Use camel case on permission name as is the standard in Zope/Plone

--- src/sc/microsite/subscribers/microsite.py
+++ src/sc/microsite/subscribers/microsite.py
@@ -3,9 +3,8 @@ from sc.microsite.config import ADD_PERMISSION
 
 
 def change_add_permission(object, event):
-    """Remove all roles from sc.microsite: Add microsite
-       permission inside a Microsite.
-       We do that to avoid having a Microsite inside a Microsite
+    """Remove all roles from 'sc.microsite: Add Microsite' permission inside
+    a Microsite. We do that to avoid having a Microsite inside a Microsite.
     """
     object.manage_permission(
         ADD_PERMISSION,
