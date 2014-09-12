b758e44cc2a123f833eb2e923dc7da58b2ab1f96
hvelarde <hector.velarde@gmail.com>
2014-09-12 11:25:18 -0300

    Move upgrade tests to a new module

--- src/sc/microsite/tests/test_setup.py
+++ src/sc/microsite/tests/test_setup.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-from plone import api
 from plone.testing.z2 import Browser
 from Products.CMFPlone.interfaces import INonInstallable
-from Products.GenericSetup.upgrade import listUpgradeSteps
 from sc.microsite.config import ADD_PERMISSION
 from sc.microsite.config import HIDDEN_PRODUCTS
 from sc.microsite.config import HIDDEN_PROFILES
@@ -14,6 +12,7 @@ import unittest
 
 
 class BaseTestCase(unittest.TestCase):
+
     """Base test case for package setup tests."""
 
     layer = INTEGRATION_TESTING
@@ -22,22 +21,25 @@ class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer['portal']
         self.qi = self.portal['portal_quickinstaller']
-        self.st = self.portal['portal_setup']
-        self.tt = self.portal['portal_types']
 
 
 class InstallTestCase(BaseTestCase):
+
     """Test package installation procedures"""
 
     def test_installed(self):
-        qi = self.qi
-        self.assertTrue(qi.isProductInstalled(PROJECTNAME))
+        self.assertTrue(self.qi.isProductInstalled(PROJECTNAME))
+
+    def test_add_permission(self):
+        roles = self.portal.rolesOfPermission(ADD_PERMISSION)
+        roles = [r['name'] for r in roles if r['selected']]
+        expected = ['Manager', 'Site Administrator', 'Owner', 'Contributor']
+        self.assertItemsEqual(roles, expected)
 
     def test_version(self):
+        setup = self.portal['portal_setup']
         self.assertEqual(
-            self.st.getLastVersionForProfile(self.profile),
-            (u'1001',)
-        )
+            setup.getLastVersionForProfile(self.profile), (u'1001',))
 
     def test_static_resource_grokker(self):
         """Grok does not register automatically the static resources anymore see:
@@ -72,81 +74,8 @@ class InstallTestCase(BaseTestCase):
         )
 
 
-class TestUpgrade(BaseTestCase):
-    """Test package upgrades."""
-
-    def get_upgrade_steps(self, source, dest):
-        upgradeSteps = listUpgradeSteps(self.st,
-                                        self.profile,
-                                        source)
-        steps = [step for step in upgradeSteps
-                 if (step[0]['dest'] == (dest,))
-                 and (step[0]['source'] == (source,))]
-        return steps
-
-    def run_upgrade(self, source, dest):
-        self.st.setLastVersionForProfile(self.profile, source)
-        # Get all upgrade steps
-        upgrade_steps = self.get_upgrade_steps(source, dest)
-        # Execute them
-        for steps in upgrade_steps:
-            if not (isinstance(steps, list)):
-                steps = [steps, ]
-            for step in steps:
-                step['step'].doStep(self.st)
-
-    def test_to1000_available(self):
-        source, dest = ('1', '1000')
-        steps = self.get_upgrade_steps(source, dest)
-        self.assertEqual(len(steps), 1)
-
-    def test_to1000_enable_ipublication(self):
-        source, dest = ('1', '1000')
-        behavior = 'plone.app.dexterity.behaviors.metadata.IPublication'
-        fti = self.tt['sc.microsite']
-        if behavior in fti.behaviors:
-            # Remove behavior if it is already there
-            behaviors = list(fti.behaviors)
-            behaviors.remove(behavior)
-            fti.behaviors = tuple(behaviors)
-        self.run_upgrade(source, dest)
-        # Get the fti again
-        fti = self.tt['sc.microsite']
-        self.assertIn(
-            behavior,
-            fti.behaviors
-        )
-
-    def test_to1001_available(self):
-        source, dest = ('1000', '1001')
-        steps = self.get_upgrade_steps(source, dest)
-        self.assertEqual(len(steps), 1)
-
-    def test_to1001_rolemap(self):
-        source, dest = ('1000', '1001')
-        self.run_upgrade(source, dest)
-        portal = self.portal
-        roles_of_permission = portal.rolesOfPermission('sc.microsite: Add Microsite')
-        roles = [role['name'] for role in roles_of_permission if role['selected']]
-        self.assertEqual(roles, ['Editor', 'Manager', 'Owner'])
-
-    def test_to1001_fix_permission(self):
-        with api.env.adopt_roles(['Manager']):
-            microsite = api.content.create(
-                container=self.portal,
-                type='sc.microsite',
-                id='microsite',
-            )
-        # Manually add Manager role to this permission
-        microsite.manage_permission(ADD_PERMISSION, roles=['Manager', ])
-        source, dest = ('1000', '1001')
-        self.run_upgrade(source, dest)
-        roles_of_permission = microsite.rolesOfPermission('sc.microsite: Add Microsite')
-        roles = [role['name'] for role in roles_of_permission if role['selected']]
-        self.assertEqual(roles, [])
-
-
 class UninstallTestCase(BaseTestCase):
+
     """Test package uninstall."""
 
     def setUp(self):
---
+++ src/sc/microsite/tests/test_upgrades.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from Products.GenericSetup.upgrade import listUpgradeSteps
+from sc.microsite.config import ADD_PERMISSION
+from sc.microsite.testing import INTEGRATION_TESTING
+
+import unittest
+
+
+class TestUpgrades(unittest.TestCase):
+
+    """Test package upgrades."""
+
+    layer = INTEGRATION_TESTING
+    profile = 'sc.microsite:default'
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.setup = self.portal['portal_setup']
+        self.types = self.portal['portal_types']
+
+    def get_upgrade_steps(self, source, dest):
+        upgradeSteps = listUpgradeSteps(self.setup, self.profile, source)
+        steps = [step for step in upgradeSteps
+                 if (step[0]['dest'] == (dest,))
+                 and (step[0]['source'] == (source,))]
+        return steps
+
+    def run_upgrade(self, source, dest):
+        self.setup.setLastVersionForProfile(self.profile, source)
+        # Get all upgrade steps
+        upgrade_steps = self.get_upgrade_steps(source, dest)
+        # Execute them
+        for steps in upgrade_steps:
+            if not (isinstance(steps, list)):
+                steps = [steps, ]
+            for step in steps:
+                step['step'].doStep(self.setup)
+
+    def test_to1000_available(self):
+        source, dest = ('1', '1000')
+        steps = self.get_upgrade_steps(source, dest)
+        self.assertEqual(len(steps), 1)
+
+    def test_to1000_enable_ipublication(self):
+        source, dest = ('1', '1000')
+        behavior = 'plone.app.dexterity.behaviors.metadata.IPublication'
+        fti = self.types['sc.microsite']
+        if behavior in fti.behaviors:
+            # Remove behavior if it is already there
+            behaviors = list(fti.behaviors)
+            behaviors.remove(behavior)
+            fti.behaviors = tuple(behaviors)
+        self.run_upgrade(source, dest)
+        # Get the fti again
+        fti = self.types['sc.microsite']
+        self.assertIn(behavior, fti.behaviors)
+
+    def test_to1001_available(self):
+        source, dest = ('1000', '1001')
+        steps = self.get_upgrade_steps(source, dest)
+        self.assertEqual(len(steps), 1)
+
+    def test_to1001_rolemap(self):
+        source, dest = ('1000', '1001')
+        self.run_upgrade(source, dest)
+        roles = self.portal.rolesOfPermission(ADD_PERMISSION)
+        roles = [r['name'] for r in roles if r['selected']]
+        expected = ['Manager', 'Site Administrator', 'Owner', 'Contributor']
+        self.assertItemsEqual(roles, expected)
+
+    def test_to1001_fix_permission(self):
+        with api.env.adopt_roles(['Manager']):
+            microsite = api.content.create(self.portal, 'sc.microsite', 'foo')
+        # simulate state at previous version
+        microsite.manage_permission(ADD_PERMISSION, roles=['Manager'])
+        # run upgrade step and verify no rol is assigned to add permission
+        source, dest = ('1000', '1001')
+        self.run_upgrade(source, dest)
+        roles = microsite.rolesOfPermission(ADD_PERMISSION)
+        roles = [r['name'] for r in roles if r['selected']]
+        self.assertEqual(roles, [])
