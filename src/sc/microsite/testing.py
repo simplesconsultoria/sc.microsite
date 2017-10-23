87dc2a0f8a6d82e5dc7014bab11922050a48443b
hvelarde <hector.velarde@gmail.com>
2017-10-23 11:39:15 -0200

    Fix Plone 5 fixture and fix tests

--- src/sc/microsite/testing.py
+++ src/sc/microsite/testing.py
@@ -1,10 +1,27 @@
 # -*- coding: utf-8 -*-
-"""Prepare test fixtures."""
+"""Setup testing infrastructure.
+
+For Plone 5 we need to install plone.app.contenttypes.
+"""
+from plone import api
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
-from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
 from plone.testing.z2 import ZSERVER_FIXTURE
+
+
+import pkg_resources
+
+
+try:
+    pkg_resources.get_distribution('plone.app.contenttypes')
+except pkg_resources.DistributionNotFound:
+    from plone.app.testing import PLONE_FIXTURE
+else:
+    from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE as PLONE_FIXTURE
+
+
+IS_PLONE_5 = api.env.plone_version().startswith('5')
 
 
 class Fixture(PloneSandboxLayer):
@@ -18,12 +35,11 @@ class Fixture(PloneSandboxLayer):
     def setUpPloneSite(self, portal):
         self.applyProfile(portal, 'sc.microsite:default')
 
+
 FIXTURE = Fixture()
+
 INTEGRATION_TESTING = IntegrationTesting(
-    bases=(FIXTURE,),
-    name='sc.microsite:Integration',
-)
+    bases=(FIXTURE,), name='sc.microsite:Integration')
+
 FUNCTIONAL_TESTING = FunctionalTesting(
-    bases=(FIXTURE, ZSERVER_FIXTURE,),
-    name='sc.microsite:Functional',
-)
+    bases=(FIXTURE, ZSERVER_FIXTURE,), name='sc.microsite:Functional')
--- src/sc/microsite/tests/test_setup.py
+++ src/sc/microsite/tests/test_setup.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
-from plone.testing.z2 import Browser
 from Products.CMFPlone.interfaces import INonInstallable
 from sc.microsite.config import ADD_PERMISSION
-from sc.microsite.config import HIDDEN_PRODUCTS
-from sc.microsite.config import HIDDEN_PROFILES
 from sc.microsite.config import PROJECTNAME
 from sc.microsite.testing import INTEGRATION_TESTING
 from zope.component import getAllUtilitiesRegisteredFor
@@ -11,21 +8,14 @@ from zope.component import getAllUtilitiesRegisteredFor
 import unittest
 
 
-class BaseTestCase(unittest.TestCase):
-
-    """Base test case for package setup tests."""
+class InstallTestCase(unittest.TestCase):
+    """Test package installation procedures"""
 
     layer = INTEGRATION_TESTING
-    profile = 'sc.microsite:default'
 
     def setUp(self):
         self.portal = self.layer['portal']
         self.qi = self.portal['portal_quickinstaller']
-
-
-class InstallTestCase(BaseTestCase):
-
-    """Test package installation procedures"""
 
     def test_installed(self):
         self.assertTrue(self.qi.isProductInstalled(PROJECTNAME))
@@ -37,22 +27,13 @@ class InstallTestCase(BaseTestCase):
         self.assertItemsEqual(roles, expected)
 
     def test_version(self):
-        setup = self.portal['portal_setup']
+        setup_tool = self.portal['portal_setup']
+        profile = 'sc.microsite:default'
         self.assertEqual(
-            setup.getLastVersionForProfile(self.profile), (u'1001',))
-
-    def test_static_resource_grokker(self):
-        """Grok does not register automatically the static resources anymore see:
-        http://svn.zope.org/five.grok/trunk/src/five/grok/meta.py?rev=123298&r1=112163&r2=123298
-        """
-        portal = self.layer['portal']
-        app = self.layer['app']
-        browser = Browser(app)
-        portal_url = portal.absolute_url()
-        browser.open('%s/++resource++sc.microsite' % portal_url)
-        self.assertEqual(browser.headers['status'], '200 Ok')
+            setup_tool.getLastVersionForProfile(profile), (u'1001',))
 
     def test_hidden_products(self):
+        from sc.microsite.config import HIDDEN_PRODUCTS
         packages = [p['id'] for p in self.qi.listInstallableProducts()]
         deps = set(HIDDEN_PRODUCTS)
         result = [p for p in deps if p in packages]
@@ -62,6 +43,7 @@ class InstallTestCase(BaseTestCase):
         )
 
     def test_hidden_profiles(self):
+        from sc.microsite.config import HIDDEN_PROFILES
         not_installable = []
         utils = getAllUtilitiesRegisteredFor(INonInstallable)
         for util in utils:
@@ -74,12 +56,14 @@ class InstallTestCase(BaseTestCase):
         )
 
 
-class UninstallTestCase(BaseTestCase):
-
+class UninstallTestCase(unittest.TestCase):
     """Test package uninstall."""
 
+    layer = INTEGRATION_TESTING
+
     def setUp(self):
-        super(UninstallTestCase, self).setUp()
+        self.portal = self.layer['portal']
+        self.qi = self.portal['portal_quickinstaller']
         self.qi.uninstallProducts(products=[PROJECTNAME])
 
     def test_uninstalled(self):
